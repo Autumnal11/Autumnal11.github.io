e815c7d6148e8ceddc3296b4e06cbdf255d09395
Autumnal11 <152314701+Autumnal11@users.noreply.github.com>
2023-12-12 08:54:06 -0800

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.lang.Math;
+ 
+public class runner_Player {
+  public static void main(String[] args) {
+    Scanner scan=new Scanner(System.in);
+    System.out.println("Hello World.");
+  }
+}
