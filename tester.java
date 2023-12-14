f1c7d0c20c4fefc18e99196ce25580e10706ed4a
Autumnal11 <152314701+Autumnal11@users.noreply.github.com>
2023-12-13 21:18:24 -0800

    Update tester.java

--- tester.java
+++ tester.java
@@ -1,9 +1,5 @@
-import java.util.Scanner;
-import java.lang.Math;
- 
 public class runner_Player {
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
     System.out.println("Hello Lennon.");
   }
 }
