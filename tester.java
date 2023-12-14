91ac1a44e5277fc677da6355bb4ad24a2f1a7a21
Autumnal11 <152314701+Autumnal11@users.noreply.github.com>
2023-12-13 21:23:48 -0800

    Update tester.java

--- tester.java
+++ tester.java
@@ -1,5 +1,5 @@
-public class runner_Player {
-  public static void main(String[] args) {
+public class runner{
+  public static void main(String[] args){
     String bart="Hello Lennon.";
     return bart;
   }
